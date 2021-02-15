872ba20454350f8fdef139361ad3605377638181
ezikechris <christophermbe1009@gmail.com>
2021-02-15 16:41:12 +0100

    Latest Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import  CardList from './card/CardList'
 import  { Robots } from './robots/Robots';
-import Search from './search/Search'
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robotos</h1>
-            <Search />
-        <CardList Robots={Robots}/>
-        </div>
+import Search from './search/Search';
+import './App.css';
+class App extends Component{
+        constructor() {
+        super()
+        this.state = {
+            Robots: Robots,
+            searchfield: ''
 
-    );
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    
+        
+    }
+    render() {
+           
+        const filteredRobots = this.state.Robots.filter(Robots =>{
+            return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
+        return (
+            <div className='tc'>
+                <h1>ThemePark</h1>
+                <Search searchChange={this.onSearchChange} />
+            <CardList Robots={filteredRobots}/>
+            </div>
+    
+        );
+    } 
+ 
 
 
 
--- src/search/Search.js
+++ src/search/Search.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const Search = () => {
+const Search = ({searchfield, searchChange}) => {
 
     return(
        <div>
             <input className="pa3 ba b-green bg-lightest-blue"
             type='search' 
-            placeholder='Search robots' />
+            placeholder='Search robots' 
+            onChange={searchChange} />
        </div>
 
 

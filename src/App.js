f83189a52204059eabc6e6006ba6d7ef63e1e41d
ezikechris <christophermbe1009@gmail.com>
2021-02-16 09:54:02 +0100

    Added Scroll Bar and Json Fake Rest Api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import  CardList from './card/CardList'
 import  { Robots } from './robots/Robots';
 import Search from './search/Search';
 import './App.css';
+import Scroll from './scroll/Scroll';
 class App extends Component{
         constructor() {
         super()
         this.state = {
-            Robots: Robots,
+            Robots: [],
             searchfield: ''
 
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({Robots:users})});
     }
 
     onSearchChange = (event) => {
@@ -23,12 +30,17 @@ class App extends Component{
         const filteredRobots = this.state.Robots.filter(Robots =>{
             return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+        if (this.state.Robots.length === 0) {
+            return <h1>loading</h1>
 
+        }else
         return (
             <div className='tc'>
                 <h1>ThemePark</h1>
                 <Search searchChange={this.onSearchChange} />
+                <Scroll> 
             <CardList Robots={filteredRobots}/>
+            </Scroll>
             </div>
     
         );
